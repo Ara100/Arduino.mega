c6d0c6f035eb50c37b693b4ac70a15b0027a71af
Araceli <ara.100.celi@gmail.com>
2019-07-30 13:24:01 -0500

    corriendo programa

--- demo1.ino
+++ demo1.ino
@@ -1,10 +1,16 @@
 
 void setup() {
   // put your setup code here, to run once:
+Serial.begin (9600);
+Serial.println("iniciando programa");
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.print("tengo flojera de programar");
+  while(1){
+    Serial.print(".");
+    delay(5000);
+  }
 
 }
